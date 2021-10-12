8c2ddd78eabbe40fc6b52cbaf34fe0d5e03685bc
Vadim Valunskii <dvvv09@gmail.com>
2021-10-12 06:57:55 +0300

    Initial commit

---
+++ src/test/java/com/valunskii/labjunit5/service/UserServiceTest.java
@@ -0,0 +1,13 @@
+package com.valunskii.labjunit5.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class UserServiceTest {
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
